e9d2534d8aae2e2d3a1add88dd9da6c0244a39a4
Memo Chou <memochou1993@gmail.com>
2024-03-31 17:13:24 +0800

    Add api stack and lambda functions

--- lib/polly-api-stack.ts
+++ lib/polly-api-stack.ts
@@ -1,16 +1,104 @@
 import * as cdk from 'aws-cdk-lib';
+import { WebSocketApi, WebSocketStage } from 'aws-cdk-lib/aws-apigatewayv2';
+import { WebSocketLambdaIntegration } from 'aws-cdk-lib/aws-apigatewayv2-integrations';
+import { BillingMode, Table } from 'aws-cdk-lib/aws-dynamodb';
+import { Architecture, Runtime } from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class PollyApiStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const table = new cdk.aws_dynamodb.Table(this, 'ConnectionTable', {
+      billingMode: BillingMode.PAY_PER_REQUEST,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      partitionKey: {
+        name: 'connectionId',
+        type: cdk.aws_dynamodb.AttributeType.STRING,
+      },
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'PollyApiQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const connectHandler = this.connectHandlerBuilder(table);
+    const disconnectHandler = this.disconnectHandlerBuilder(table);
+    const sendMessageHandler = this.sendMessageHandlerBuilder(table);
+    const defaultHandler = this.defaultHandlerBuilder();
+
+    const webSocketApi = new WebSocketApi(this, 'PollyWebSocketApi', {
+      routeSelectionExpression: '$request.body.action',
+      connectRouteOptions: {
+        integration: new WebSocketLambdaIntegration('ConnectIntegration', connectHandler),
+      },
+      disconnectRouteOptions: {
+        integration: new WebSocketLambdaIntegration('DisconnectIntegration', disconnectHandler),
+      },
+      defaultRouteOptions: {
+        integration: new WebSocketLambdaIntegration('DefaultIntegration', defaultHandler),
+      },
+    });
+
+    webSocketApi.addRoute('send-message', {
+      integration: new WebSocketLambdaIntegration('SendMessageIntegration', sendMessageHandler),
+    });
+    webSocketApi.grantManageConnections(sendMessageHandler);
+    webSocketApi.grantManageConnections(defaultHandler);
+
+    new WebSocketStage(this, 'PollyProductionStage', {
+      webSocketApi,
+      stageName: 'production',
+      autoDeploy: true,
+    });
+  }
+
+  connectHandlerBuilder(table: Table) {
+    const handler = new cdk.aws_lambda_nodejs.NodejsFunction(this, 'ConnectHandler', {
+      environment: {
+        TABLE_NAME: table.tableName,
+      },
+      architecture: Architecture.ARM_64,
+      runtime: Runtime.NODEJS_20_X,
+      entry: 'lambda/connect-handler.ts',
+    });
+
+    table.grantWriteData(handler);
+
+    return handler;
+  }
+
+  disconnectHandlerBuilder(table: Table) {
+    const handler = new cdk.aws_lambda_nodejs.NodejsFunction(this, 'DisconnectHandler', {
+      environment: {
+        TABLE_NAME: table.tableName,
+      },
+      architecture: Architecture.ARM_64,
+      runtime: Runtime.NODEJS_20_X,
+      entry: 'lambda/disconnect-handler.ts',
+    });
+
+    table.grantWriteData(handler);
+
+    return handler;
+  }
+
+  sendMessageHandlerBuilder(table: Table) {
+    const handler = new cdk.aws_lambda_nodejs.NodejsFunction(this, 'SendMessageHandler', {
+      environment: {
+        TABLE_NAME: table.tableName,
+      },
+      architecture: Architecture.ARM_64,
+      runtime: Runtime.NODEJS_20_X,
+      entry: 'lambda/send-handler.ts',
+    });
+
+    table.grantReadWriteData(handler);
+
+    return handler;
+  }
+
+  defaultHandlerBuilder() {
+    return new cdk.aws_lambda_nodejs.NodejsFunction(this, 'DefaultHandler', {
+      architecture: Architecture.ARM_64,
+      runtime: Runtime.NODEJS_20_X,
+      entry: 'lambda/default-handler.ts',
+    });
   }
 }
